290910a49de1beeeaeb1e54068cb086b24515580
Gutherie <jasonbaker@ieee.org>
2015-08-19 10:57:54 -0400

    testing

--- GutherieSurvey/src/gutherie/ReadMe.java
+++ GutherieSurvey/src/gutherie/ReadMe.java
@@ -3,6 +3,9 @@ package gutherie;
 public class ReadMe {
 	private ReadMe(){}
 	
+	/*
+	 * General info about the app
+	 */
 	public static void getReadme(){
 		System.out.println("Gutherie Survey Software v0.1");
 	}
